f7d3f4ee3506380da0ae814df82aacbfc889e1ba
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2020-09-11 23:02:55 +0530

    Cookies

--- coursera/conFusionServer/app.js
+++ coursera/conFusionServer/app.js
@@ -34,32 +34,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-function auth (req, res, next) {
-	console.log(req.headers);
-	var authHeader = req.headers.authorization;
-	if (!authHeader) {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		next(err);
-		return;
-	}
-
-	var auth = new Buffer.from(authHeader.split(' ')[ 1 ], 'base64').toString().split(':');
-	var user = auth[ 0 ];
-	var pass = auth[ 1 ];
-	if (user == 'admin' && pass == 'password') {
-		next(); //authorized
-	} else {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		next(err);
-	}
-}
-
+app.use(cookieParser('12345-67890-09876-54321'));
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -70,12 +45,47 @@ app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
 	next(createError(404));
 });
-
 // error handler
-app.use(function (err, req, res, next) {
+app.use(errorHandler);
+
+function auth (req, res, next) {
+	if (!req.signedCookies.user) {
+		var authHeader = req.headers.authorization;
+		if (!authHeader) {
+			var err = new Error('You are not authenticated!');
+			res.setHeader('WWW-Authenticate', 'Basic');
+			err.status = 401;
+			next(err);
+			return;
+		}
+		var auth = new Buffer.from(authHeader.split(' ')[ 1 ], 'base64').toString().split(':');
+		var user = auth[ 0 ];
+		var pass = auth[ 1 ];
+		if (user == 'admin' && pass == 'password') {
+			res.cookie('user', 'admin', { signed: true });
+			next(); //authorized
+		} else {
+			var err = new Error('You are not authenticated!');
+			res.setHeader('WWW-Authenticate', 'Basic');
+			err.status = 401;
+			next(err);
+		}
+	} else {
+		if (req.signedCookies.user === 'admin') {
+			next();
+		} else {
+			var err = new Error('You are not authenticated!');
+
+			err.status = 401;
+			next(err);
+		}
+	}
+}
+
+function errorHandler (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -83,6 +93,6 @@ app.use(function (err, req, res, next) {
 	// render the error page
 	res.status(err.status || 500);
 	res.render('error');
-});
+}
 
 module.exports = app;
